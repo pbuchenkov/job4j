10889d25efecdfa34e613169988b917fab05c849
pbuchenkov <pavel.buchenkov@gmail.com>
2018-12-10 12:09:18 +0300

    1.3. JavaDoc. Code convention[#81997]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate.
+ *
+ * @author Pavel Buchenkov (pavel.buchenkov@gmail.com)
+ 
+public class Calculate {
+	/**
+	* Конструктор, вывод строки в консоль.
+	* "param arg - arg.
+	*/
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
