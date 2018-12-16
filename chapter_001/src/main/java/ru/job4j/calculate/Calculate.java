417cc7526b206254676efb89c483df8cc6111c5d
pbuchenkov <pavel.buchenkov@gmail.com>
2018-12-16 11:54:00 +0300

    2.2. Импорт Maven проекта.[#81953]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,22 +6,22 @@ package ru.job4j.calculate;
  * @author Pavel Buchenkov (pavel.buchenkov@gmail.com)
  * @version $Id$
  * @since 0.1
-*/
-public class Calculate { 
-	/**
-	* Main.
-	* @param args - args
-    */
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-	
-	/**
+
+    /**
      * Method echo.
      * @param name Pavel Buchenkov.
      * @return Echo plus Pavel Buchenkov.
      */
     public String echo(String name) {
         return "Echo, echo, echo : " + name;
-	}	
+    }
 }
